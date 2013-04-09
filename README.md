e7f34f9d185413f42b29d6f20351ad385995391e
marazmista <marazmista@gmail.com>
2013-04-09 14:43:21 +0200

    few improvements, can read config by filepath in first argument

--- README.md
+++ README.md
@@ -5,12 +5,17 @@ At first I wanted little program, that shutdown my pc when all night downloads h
 
 # Options
 -----------
+You can pass config file in fist parameter: `simple-netstat /path/to/config/configfilename`.
+
+* __Mode__:
+	* __1 - Monitor:__ just watch network and store data in logfile.
+	* __2 - ShutdownApp:__ closes program when reaches Positive idle speed pases.
+	* __3 - ShutdownPC:__ shutdown pc when reaches Positive idle speed pases. This is done by command `sudo shutdown -hP now`.
 * __Interface:__ network interface to monitor. `cat /sys/class/net/` to find out what interfaces available.
 * __Interval:__ [seconds] how often program will read network data from sys files.
 * __Idle speed:__ [kilobytes per interval] how many kilobytes is transffered in given interval. 
-* __Positive idle speed passes:__ how many times in a row program find out that notwork traffic is below Idle Speed, and then shutdown pc. This option is ignored in Monitor mode.
+* __Positive idle speed passes:__ how many times in a row program find out that notwork traffic is below Idle Speed, and then shutdown pc.
 * __Verbose:__ [0-1] be verbose or only log file?
-* __Monitor mode:__ [0-1] only watch network traffic, disables shutdown feature.
 
 
 # Log file
@@ -29,7 +34,7 @@ Log file is something like this (ignore second line):
 
 format:
 
-	hour;downloaded kB/interval;uploaded kB/interval;download speed below Idle Speed;upload speed below Idle Speed
+	Hour;Downloaded kB/interval;Uploaded kB/interval;Download speed below Idle Speed;Ipload speed below Idle Speed
 
 You can paste log file to Calc or Excel to make charts or stuff like that.
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,51 +17,35 @@ void watchNetwork();
 void pcOff();
 int readValue(double&, string);
 string timeNow(bool);
+bool selectInterface();
+void askVerbose();
+void manualConfig();
+void saveConfig();
+bool checkExistingConfig(string&, bool readOnlyMode);
+void askInterval();
 
-static short readLag,idleSpeed,idleCheckTriesRX,idleCheckTriesTX;
-static string netInterface,rxtxFilePath;
+static short readLag,idleSpeed,idleCheckTriesRX,idleCheckTriesTX,mode;
+static string netInterface,rxtxFilePath,configPath = "config";
 static double rxLast_, txLast_;
-bool verbose,monitorMode;
+bool verbose;
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc > 1)
+        configPath = argv[1];
+
     initialSetup();
     watchNetwork();
 
-    if (!monitorMode)
+    if (mode == 3)
         pcOff();
 
+    cout << "Bye!" << endl;
     return 0;
 }
 
-void initialSetup() {
-    ifstream configFile;
-    configFile.open("config");
-    if (configFile.is_open()) {
-        cout << endl << "* Great, found config file, reading..." << endl;
-
-        configFile >> netInterface;
-        rxtxFilePath = "/sys/class/net/"+ netInterface + "/statistics/";
-        configFile >> readLag;
-        configFile >> idleSpeed;
-        configFile >> idleCheckTriesRX;
-        idleCheckTriesTX = idleCheckTriesRX;
-        configFile >> verbose;
-        configFile >> monitorMode;
-
-        cout << "* Current config: \n" << "* Interface: " << netInterface << endl
-             << "* Interval: " << readLag << endl
-             << "* Idle speed: " << idleSpeed << endl
-             << "* Passes: " << idleCheckTriesRX << endl
-             << "* Verbose: " << verbose << endl
-             << "* Monitor mode " << monitorMode << endl;
-
-        configFile.close();
-        return;
-    }
-
-    cout << endl << "* Config file not found, manual setup." << endl;
-
+bool selectInterface()
+{
     cout << "Network interface: ";
     cin >> netInterface;
     rxtxFilePath = "/sys/class/net/"+ netInterface + "/statistics/";
@@ -70,67 +54,160 @@ void initialSetup() {
     fch.open(rxtxFilePath+"rx_bytes");
     if (!fch.is_open()) {
         cout << "! Oppps, can't find that interface." << endl;
-        return;
+        return false;
     }
     fch.close();
-
     cout << "* Looks OK, carry on." << endl;
-    cout << "Read interface data interval [s]: ";
-    cin >> readLag;
-    cout << "Idle speed [kB/interval]: ";
-    cin >> idleSpeed;
-    cout<< "Positive idle speed passes (meaningless in monitor mode): ";
-    cin >> idleCheckTriesTX;
-    idleCheckTriesRX = idleCheckTriesTX;
+
+    return true;
+}
+
+void askVerbose()
+{
     cout << "Verbose mode? (0-1): ";
     cin >> verbose;
-    cout << "Monitor mode? (disable shutdown pc when network traffic drops to nothing) (0-1): ";
-    cin >> monitorMode;
+}
 
+void askInterval()
+{
+    cout << "Read interface data interval [s]: ";
+    cin >> readLag;
+}
+
+void manualConfig()
+{
+    if (selectInterface()) {
+        askInterval();
+        cout << "Idle speed [kB/interval]: ";
+        cin >> idleSpeed;
+        cout<< "Positive idle speed passes: ";
+        cin >> idleCheckTriesTX;
+        idleCheckTriesRX = idleCheckTriesTX;
+        askVerbose();
+    } else {
+        exit(-1);
+    }
+}
+
+void saveConfig()
+{
     bool sConfig;
     cout << "Save config? (0-1)";
     cin >> sConfig;
 
     if (sConfig) {
+        string cName;
+        cout << "Config name: ";
+        cin >> cName;
         ofstream configFile;
-        configFile.open("config");
+        configFile.open(cName);
         if (configFile.is_open()) {
+            configFile << mode << endl;
             configFile << netInterface << endl;
             configFile << readLag << endl;
             configFile << idleSpeed << endl;
             configFile << idleCheckTriesRX << endl;
             configFile << verbose << endl;
-            configFile << monitorMode <<endl;
 
             configFile.close();
-            cout << "Config saved. Search for it in app dir." << endl;
+            cout << "Config saved." << endl;
 
         } else
             cout << "! Opps, can't save config file." << endl;
     }
 }
 
+bool checkExistingConfig(string &cFilePath, bool readOnlyMode)
+{
+    ifstream configFile;
+    configFile.open(cFilePath);
+    if (configFile.is_open()) {
+        cout << endl << "* Great, found config file, reading..." << endl;
+
+        if (readOnlyMode) {
+            configFile >> mode;
+            configFile.close();
+            return true;
+        }
+
+        configFile >> mode;
+        configFile >> netInterface;
+        rxtxFilePath = "/sys/class/net/"+ netInterface + "/statistics/";
+        configFile >> readLag;
+        configFile >> idleSpeed;
+        configFile >> idleCheckTriesRX;
+        idleCheckTriesTX = idleCheckTriesRX;
+        configFile >> verbose;
+
+        cout << "* Current config: \n"
+             << "* Mode: " << mode << endl
+             << "* Interface: " << netInterface << endl
+             << "* Interval: " << readLag << endl
+             << "* Idle speed: " << idleSpeed << endl
+             << "* Passes: " << idleCheckTriesRX << endl
+             << "* Verbose: " << verbose << endl;
+
+        configFile.close();
+        return true;
+    } else
+        return false;
+}
+
+void initialSetup() {
+    if (!configPath.empty()) {
+        checkExistingConfig(configPath,true);
+    } else {
+        cout << endl << "* Select mode [1 - monitor; 2 - shutdownApp; 3 - shutdownPC]: " << endl << "> ";
+        cin >> mode;
+    }
+
+    switch (mode) {
+    case 1:
+        selectInterface();
+        askInterval();
+        askVerbose();
+        break;
+    case 2:
+        if (!checkExistingConfig(configPath,false)) {
+            manualConfig();
+            saveConfig();
+        }
+        break;
+    case 3:
+        if (!checkExistingConfig(configPath, false)) {
+            manualConfig();
+            saveConfig();
+        }
+    default:
+        cout << "! Bad mode selected. Exit." << endl;
+        exit(-1);
+        break;
+    }
+}
+
 void watchNetwork() {
     ofstream logFile;
     logFile.open(timeNow(true)+"_sessionLog");
-
     if (!logFile.is_open())
         cout << "! Error saving session log file. Proceeding without log file." << endl;
 
-    logFile << "Interface: " << netInterface << "  interval: " << readLag << " idleSpeed: " << idleSpeed << endl;
+    logFile << "Interface: " << netInterface << "  interval: " << readLag << endl;
 
     cout << "-------------------" << endl
          << "* Cool, setup complete."<< endl
          << "* Launching network interface monitoring..." << endl;
 
-    ushort txPass(0),rxPass(0); // cunting how many times traffic speed was below idleSpeed //
     int rxDiff,txDiff;
+    ushort txPass(0),rxPass(0); // cunting how many times traffic speed was below idleSpeed //
+
+    if (mode == 1)
+        idleCheckTriesRX = idleCheckTriesTX = 1;
 
     while ((idleCheckTriesRX > rxPass) || (idleCheckTriesTX > txPass)) {
         rxDiff = readValue(rxLast_,"rx_bytes");
         txDiff = readValue(txLast_,"tx_bytes");
 
-        if (!monitorMode) {
+        if (mode != 1) {
             if (rxDiff < idleSpeed)
                 rxPass++;
             else
@@ -183,7 +260,7 @@ int readValue(double &lastValue, string filename) {
 }
 
 void pcOff() {
-   // string cmd("echo \"power off\"");
+    //string cmd("echo \"power off\"");
     string cmd("sudo shutdown -hP now");
     system(cmd.c_str());
 }
